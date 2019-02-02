ab3444299c81653aab8f58c247cffbf86121f006
Mahesh <mahesh.n@tonguestun.in>
2019-02-02 19:52:45 +0530

    user auth

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const { User } = require('../models/user')
 
 
+// user sign up
 router.post('/', function(req, res){
-    console.log('users Controller');
-    
     let body = req.body
     let user = new User(body)
     user.save().then(function(user){
-        res.send(user)
+      return user.generateToken()
+    })
+    .then(function(token){
+        res.header('x-auth', token).send()
     })
     .catch(function(err){
         res.send(err)
@@ -17,6 +19,25 @@ router.post('/', function(req, res){
 })
 
 
+// login
+// post /users/login
+router.post('/login', function(req, res){
+    let body = req.body; 
+    console.log(body);
+    
+    User.findByCredentials(body.email, body.password).then(function(user){
+        return user.generateToken()
+    })
+    .then((token) => {
+        res.header('x-auth', token).send()
+    })
+    .catch(function(err){
+        res.status(401).send(err)
+    })
+})
+
+
+// users list
 router.get('/', function(req, res){
     console.log('users ');
     User.find().then(function(users){
--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,124 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+const validatePackage = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = new Schema ({
-    email: {
-        type: String
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 128,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: function(value) {
+        return validatePackage.isEmail(value)
+      },
+      message: function() {
+        return 'invalid email format'
+      }
     }
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: function(value){
+        return validatePackage.isNumeric(value)
+      },
+      message: function(){
+        return 'invalid mobile format'
+      }
+    }
+  },
+  tokens: [
+    {
+      token: {
+        type: String
+      }
+    }
+  ]
 })
+
+
+
+userSchema.methods.generateToken = function(){
+    const user = this
+    const tokenData = {
+        userId: user._id
+    }
+    const token = jwt.sign(tokenData, 'moneycop')
+    user.tokens.push({
+        token
+    })
+    return user.save().then((user) => {
+        return Promise.resolve(token)
+    })
+}
+
+userSchema.statics.findByToken = function(token){
+  let User = this
+  let tokenData
+  try {
+    tokenData = jwt.verify(token, 'moneycop')
+  } catch (e) {
+    return Promise.reject(e)
+  }
+
+  return User.findOne({
+    '_id': tokenData.userId,
+    'tokens.token': token
+  })
+
+}
+
+
+
+userSchema.statics.findByCredentials = function(email, password){
+  let User = this 
+  return User.findOne({ email: email}).then(function(user){
+      if(!user){
+          return Promise.reject('invalid email ')
+          // return new Promise(function(resolve, reject){
+          //     reject('invalid email or password')
+          // })
+      }
+
+      // return new Promise(function(resolve, reject){
+      //     bcrypt.compare(password, user.password).then(function(res){
+      //         if(res) {
+      //             resolve(user)
+      //         } else {
+      //             reject('invalid password')
+      //         }
+      //     })
+      // })
+      
+      return bcrypt.compare(password, user.password).then(function(result){
+          if(result) {
+            return Promise.resolve(user)
+          } else {
+            console.log(result);
+            
+            return Promise.reject('invalid email or password')
+          }
+      })
+
+      // [] / new Array()
+      // {} / new Object()
+
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
